97ed4e4c2c34c8933e0353c8578c59128f7b506a
Gbosikov <g.bosikov@gmail.com>
2022-02-05 01:20:04 +0400

    lesson_02

---
+++ Bosikov_Garik_dz_02/Task_2_5.py
@@ -0,0 +1,55 @@
+from random import uniform
+
+
+def transfer_list_in_str(list_in: list) -> str:
+    """Преобразует каждый элемент списка (вещественное число) в строку вида '<r> руб <kk> коп' и
+        формирует из них единую строковую переменную разделяя значения запятой."""
+    list_out = []
+
+    for ind, value in enumerate(my_list):
+        float_to_str = str(f'{float(value):.2f}').split('.')
+        list_out.append(''.join(f'{float_to_str[0]} руб {float_to_str[1]} коп'))
+
+    str_out = ', '.join(list_out)
+    return str_out
+
+
+my_list = [round(uniform(10, 100), 2) for _ in range(1, 16)]  # автоматическая генерация случайных 15 чисел
+print(f'Исходный список: {my_list}')
+result_1 = transfer_list_in_str(my_list)
+print(result_1)
+
+
+def sort_prices(list_in: list) -> list:
+    """Сортирует вещественные числа по возрастанию, не создавая нового списка"""
+    list_in.sort()
+    return list_in
+
+
+# зафиксируйте здесь информацию по исходному списку my_list
+result_2 = sort_prices(my_list)
+# зафиксируйте здесь доказательство, что результат result_2 остался тем же объектом
+print(result_2)
+
+
+def sort_price_adv(list_in: list) -> list:
+    """Создаёт новый список и возвращает список с элементами по убыванию"""
+
+    list_out = list(reversed(list_in))
+    return list_out
+
+
+result_3 = sort_price_adv(my_list)
+print(result_3)
+
+
+def check_five_max_elements(list_in: list) -> list:
+    """Проверяет элементы входного списка вещественных чисел и возвращает
+        список из ПЯТИ максимальных значений"""
+    # пишите реализацию здесь
+    list_out = list(reversed(list_in))[:5:]
+    return list_out
+
+
+result_4 = check_five_max_elements(my_list)
+print(result_4)

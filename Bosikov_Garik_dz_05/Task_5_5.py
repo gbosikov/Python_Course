162aa8c4b33b8ea69fca52b71b66902a51a67eba
Gbosikov <g.bosikov@gmail.com>
2022-02-15 22:33:42 +0400

    lesson_05

--- Bosikov_Garik_dz_05/Task_5_5.py
+++ Bosikov_Garik_dz_05/Task_5_5.py
@@ -1,9 +1,26 @@
 def get_uniq_numbers(src: list):
-    return [val for val in src if src.count(val) == 1]
+    unique = set()
+    tmp = set()
+    for el in src:
+        if el not in tmp:
+            unique.add(el)
+        else:
+            unique.discard(el)
+        tmp.add(el)
+
+    return list(unique)
 
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 print(*get_uniq_numbers(src))
 
 
-
+# unique = set()
+# tmp = set()
+# for el in src:
+#     if el not in tmp:
+#         unique.add(el)
+#     else:
+#         unique.discard(el)
+#     tmp.add(el)
+# print(unique)

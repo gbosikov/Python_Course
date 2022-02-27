0018698dfc6fa220aad9e79e8e3532c576375f5b
Gbosikov <g.bosikov@gmail.com>
2022-02-27 21:35:08 +0400

    lesson_07

--- Bosikov_Garik_dz_07/Test.py
+++
@@ -1,8 +0,0 @@
-rainbow = {'red': 1}
-print(rainbow)
-# Update by passing dictionary 
-new_key_values_dict = {'orange': 2, 'yellow': 3}
-rainbow.update(new_key_values_dict)
-print("update by passing dictionary")
-print(rainbow)
-

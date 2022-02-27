dc7a1c307788ff15b9aaa93fd301f05c29c7a6ea
Gbosikov <g.bosikov@gmail.com>
2022-02-27 21:40:25 +0400

    lesson_07

--- Bosikov_Garik_dz_07/Task_7_4.py
+++ Bosikov_Garik_dz_07/Task_7_4.py
@@ -1,27 +1,25 @@
-from os import walk
-from os.path import join
-from os.path import getsize
 import os
+from Task_7_1 import BASE_DIR
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def file_size_list(dir_path:str)->list:
+def file_size_list(dir_path: str) -> list:
     """
     Принемает в качестве аргумента пкть до директории
     :param dir_path: Путь до директории
     :return: список файлов размер которых больше 0
     """
     size = []
-    for root, dirs, files in walk(dir_path):
+    for root, dirs, files in os.walk(dir_path):
         if not files:
             continue
-        size.extend(getsize(join(root, name)) for name in files if int(getsize(join(root, name)) > 0))
+        size.extend(os.path.getsize(os.path.join(root, name))
+                    for name in files if int(os.path.getsize(os.path.join(root, name)) > 0))
     return size
 
 # print(file_size_list(BASE_DIR))
 
 size_range = [10 ** i for i in range(2, len(file_size_list(BASE_DIR)) + 1)]
-dict_out = {}
+dict_out = dict()
 for item in file_size_list(BASE_DIR):
     for key in size_range:
         if item < key:
